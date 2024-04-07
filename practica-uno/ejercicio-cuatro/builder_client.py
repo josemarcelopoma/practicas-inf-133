8845a5a7e343fe30c3d291ff495c55c3e377dea1
josemarcelopoma <153699804+josemarcelopoma@users.noreply.github.com>
2024-04-07 01:19:31 +0000

    ejercicio4

--- practica-uno/ejercicio-cuatro/builder_client.py
+++ practica-uno/ejercicio-cuatro/builder_client.py
@@ -1,56 +1,54 @@
 import requests
 import json
-
 url = "http://localhost:8000/patients"
 headers = {'Content-type': 'application/json'}
 
-# Crear un paciente
 print("Creando un nuevo paciente:")
-nuevo_paciente = {
-    "nombre": "Juan",
-    "apellido": "Perez",
-    "edad": 30,
+n_paciente = {
+    "nombre": "Rodrigo",
+    "apellido": "Ticona",
+    "edad": 43,
     "genero": "Masculino",
-    "diagnostico": "Resfriado",
-    "doctor": "Dr.Garcia"  # Añadir el atributo "doctor"
+    "diagnostico": "tuberculosis",
+    "doctor": "Dr.Yujra" 
 }
-response = requests.post(url, json=nuevo_paciente, headers=headers)
-print(response.json())
 
-# Listar todos los pacientes
-print("\nListando todos los pacientes:")
+response = requests.post(url, json=n_paciente, headers=headers)
+print(response.json())
+print("Creando un nuevo paciente:")
+n_paciente = {
+    "nombre": "Roberto",
+    "apellido": "Pacoricona",
+    "edad": 21,
+    "genero": "Masculino",
+    "diagnostico": " aterosclerosis",
+    "doctor": "Dr.Calle" 
+}
+
+response = requests.post(url, json=n_paciente, headers=headers)
+print(response.json())
+print("\n------Listando pacientes:------")
 response = requests.get(url, headers=headers)
 print(response.json())
 
-# Buscar pacientes por CI
-print("\nBuscando paciente por CI:")
+print("\n-----Paciente por CI:-----")
 response = requests.get(url + "/1", headers=headers)
 print(response.json())
-
-# Listar a los pacientes que tienen diagnostico de `Diabetes`
-print("\nListando pacientes con diagnóstico de Diabetes:")
+print("\n------Pacientes por diagnostico= diabetes:-----")
 response = requests.get(url + "?diagnostico=Diabetes", headers=headers)
 print(response.json())
-
-# Listar a los pacientes que atiende el Doctor `Pedro Pérez`
-print("\nListando pacientes atendidos por el Doctor Pedro Pérez:")
-response = requests.get(url + "?doctor=Pedro Pérez", headers=headers)
-print(response.json())
-
-# Actualizar la información de un paciente
-print("\nActualizando la información de un paciente:")
+print("\n------Actualizando un paciente:------")
 nuevo_datos_paciente = {
-    "nombre": "Juanita",
-    "apellido": "Perez",
-    "edad": 30,
-    "genero": "Femenino",
-    "diagnostico": "Resfriado",
-    "doctor": "Dr.Garcia"
+    "nombre": "Adrian",
+    "apellido": "Ticona",
+    "edad": 43,
+    "genero": "Masculino",
+    "diagnostico": "tuberculosis",
+    "doctor": "Dr.Yujra"
 }
 response = requests.put(url + "/1", json=nuevo_datos_paciente, headers=headers)
 print(response.json())
 
-# Eliminar un paciente
-print("\nEliminando un paciente:")
+print("\n------Eliminando paciente:------")
 response = requests.delete(url + "/1")
 print(response.json())
--- practica-uno/ejercicio-cuatro/builder_server.py
+++ practica-uno/ejercicio-cuatro/builder_server.py
@@ -1,25 +1,22 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-
 from urllib.parse import urlparse, parse_qs
-
 patients = {}
-class Patient:
+class Pacien1:
     def __init__(self):
         self.nombre = None
         self.apellido = None
         self.edad = 0
         self.genero = None
         self.diagnostico = None
-        self.doctor = None  # Agregar el atributo doctor
+        self.doctor = None  
 
     def __str__(self):
-        return f"Nombre: {self.nombre}, Apellido: {self.apellido}, Edad: {self.edad}, Género: {self.genero}, Diagnóstico: {self.diagnostico}, Doctor: {self.doctor}"
+        return f"Nombre: {self.nombre}, Apellido: {self.apellido},Edad: {self.edad}, Género: {self.genero},Diagnóstico: {self.diagnostico}, Doctor: {self.doctor}"
 
-class PatientBuilder:
+class Paciente_builder:
     def __init__(self):
-        self.patient = Patient()
-        
+        self.patient = Pacien1()
     def set_nombre(self, nombre):
         self.patient.nombre = nombre
     
@@ -35,72 +32,61 @@ class PatientBuilder:
     def set_diagnostico(self, diagnostico):
         self.patient.diagnostico = diagnostico
     
-    def set_doctor(self, doctor):  # Agregar el método set_doctor
+    def set_doctor(self, doctor):  
         self.patient.doctor = doctor
     
     def get_patient(self):
         return self.patient
-    
 class Hospital:
     def __init__(self, builder):
         self.builder = builder
-        
-    def create_patient(self, nombre, apellido, edad, genero, diagnostico, doctor):
+    def Crear_paciente(self, nombre, apellido, edad, genero, diagnostico, doctor):
         self.builder.set_nombre(nombre)
         self.builder.set_apellido(apellido)
         self.builder.set_edad(edad)
         self.builder.set_genero(genero)
         self.builder.set_diagnostico(diagnostico)
-        self.builder.set_doctor(doctor)  # Configurar el doctor en el builder
+        self.builder.set_doctor(doctor)  
         return self.builder.get_patient()
-
-class PatientService(BaseHTTPRequestHandler):
+class Paciente_servicio(BaseHTTPRequestHandler):
     def __init__(self):
-        self.builder = PatientBuilder()
+        self.builder = Paciente_builder()
         self.hospital = Hospital(self.builder)
-        
-    
     def handle_post_request(self, post_data):
         nombre = post_data.get("nombre", None)
         apellido = post_data.get("apellido", None)
         edad = post_data.get("edad", None)
         genero = post_data.get("genero", None)
         diagnostico = post_data.get("diagnostico", None)
-        doctor = post_data.get("doctor", None)  # Obtener el valor del doctor del post_data
-        
-        patient = self.hospital.create_patient(nombre, apellido, edad, genero, diagnostico, doctor)
+        doctor = post_data.get("doctor", None)  
+        patient = self.hospital.Crear_paciente(nombre, apellido, edad, genero, diagnostico, doctor)
         patients[len(patients) + 1] = patient
         return patient
-    
-    def read_patients(self):
+    def leer_paciente(self):
         return {index: patient.__dict__ for index, patient in patients.items()}
-    
-    def find_patient_by_CI(self, CI):
+    def buscar_paciente_ci(self, CI):
         for i, patient in patients.items() :
             if i == CI:
                 return patient
-            
-    def delete_patient(self, index):
+    def Eliminar_paciente(self, index):
         if index in patients:
             return patients.pop(index)
         else:
             return None
-
-    def find_by_doctor(self, doctor):
+    def buscar_doctor(self, doctor):
         patientD = []
         for i, patient in patients.items():
             if patient.doctor == doctor:
-                patientD.append(patient.__dict__)  # Agregar el diccionario que representa al paciente
+                patientD.append(patient.__dict__)  
         return patientD
 
-    
-    def find_by_diagnostic(self, diagnostic):
+    def BUscar_paciente(self, diagnostic):
         patientD = []
         for i, patient in patients.items():
             if patient.diagnostico == diagnostic:
-                patientD.append(patient.__dict__)  # Agregar el diccionario que representa al paciente
+                patientD.append(patient.__dict__)  
         return patientD
-    def put_patient(self, index, data):
+    def Poner_paciente(self, index, data):
         if index in patients:
             patient = patients[index]
             nombre = data.get("nombre", None)
@@ -146,7 +132,7 @@ class HTTPDataHandler:
 
 class PatientHandler(BaseHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
-        self.controller = PatientService()
+        self.controller = Paciente_servicio()
         super().__init__(*args, **kwargs)
         
         
@@ -155,33 +141,33 @@ class PatientHandler(BaseHTTPRequestHandler):
         query_params = parse_qs(parsed_path.query)
         
         if self.path == "/patients":
-            response_data = self.controller.read_patients()
+            response_data = self.controller.leer_paciente()
             HTTPDataHandler.handle_response(self, 200, response_data)
         elif parsed_path.path == "/patients":
             if "diagnostico" in query_params:
                 diagnostico = query_params["diagnostico"][0]
-                patient_filtred = self.controller.find_by_diagnostic(diagnostico)
+                patient_filtred = self.controller.BUscar_paciente(diagnostico)
                 print(patient_filtred)
                 if patient_filtred: HTTPDataHandler.handle_response(self, 200, patient_filtred)
-                else: HTTPDataHandler.handle_response(self, 404, {"Error":"patient not found"})
+                else: HTTPDataHandler.handle_response(self, 404, {"Error":"paciente no encontrado"})
             elif "doctor" in query_params:
                 doctor = query_params["doctor"][0]
-                patient_filtred = self.controller.find_by_doctor(doctor)
+                patient_filtred = self.controller.buscar_doctor(doctor)
                 if patient_filtred: HTTPDataHandler.handle_response(self, 200, patient_filtred)
-                else: HTTPDataHandler.handle_response(self, 404, {"Error":"patient not found"})
+                else: HTTPDataHandler.handle_response(self, 404, {"Error":"paciente no encontrado"})
             else:
                 HTTPDataHandler.handle_response(self, 404, patients)
             
         elif self.path.startswith("/patients/"):
             index = int(self.path.split("/")[2])
-            find_patient = self.controller.find_patient_by_CI(index)
+            find_patient = self.controller.buscar_paciente_ci(index)
             if find_patient:
                 HTTPDataHandler.handle_response(self, 200, find_patient.__dict__)
             else: 
-                HTTPDataHandler.handle_response(self, 404, {"Error": "Patient not found"})
+                HTTPDataHandler.handle_response(self, 404, {"Error": "paciente no encontrado"})
 
         else:
-            HTTPDataHandler.handle_response(self, 404, {"Error": "Ruta no existente"})
+            HTTPDataHandler.handle_response(self, 404, {"Error": "la Ruta no existente"})
 
 
     def do_POST(self):
@@ -191,33 +177,38 @@ class PatientHandler(BaseHTTPRequestHandler):
             
             HTTPDataHandler.handle_response(self, 201, response_data.__dict__)
         else:
-            HTTPDataHandler.handle_response(self, 404, {"Error": "Ruta no existente"})
+            HTTPDataHandler.handle_response(self, 404, {"Error": "la Ruta no existente"})
+            
     def do_DELETE(self):
         if self.path.startswith("/patients/"):
             index = int(self.path.split("/")[2])
-            delete_patient = self.controller.delete_patient(index)
-            if delete_patient:
-                HTTPDataHandler.handle_response(self, 200, delete_patient.__dict__)
-            else: HTTPDataHandler.handle_response(self, 404, {"Error":"No se encontro a un paciente con ese CI"})
+            Eliminar_paciente = self.controller.Eliminar_paciente(index)
+            if Eliminar_paciente:
+                HTTPDataHandler.handle_response(self, 200, Eliminar_paciente.__dict__)
+            else: HTTPDataHandler.handle_response(self, 404, {"Error":"paciente con ci no encontrado"})
         else:
-            HTTPDataHandler.handle_response(self, 404, {"Error": "Ruta no existente"})
+            HTTPDataHandler.handle_response(self, 404, {"Error": "la Ruta no existente"})
+
     def do_PUT(self):
         if self.path.startswith("/patients/"):
-            index = int(self.path.split("/")[2])  # Usamos 'index' en lugar de 'CI'
+            index = int(self.path.split("/")[2]) 
             data = HTTPDataHandler.handle_reader(self)
-            response_put = self.controller.put_patient(index, data)  # Pasamos 'index' a 'put_patient'
+            response_put = self.controller.Poner_paciente(index, data)  
             if response_put:
                 HTTPDataHandler.handle_response(self, 200, response_put.__dict__)
             else:
-                HTTPDataHandler.handle_response(self, 404, {"Error": "Paciente no encontrado"})
+                HTTPDataHandler.handle_response(self, 404, {"Error": " no encontrado"})
         else:
-            HTTPDataHandler.handle_response(self, 404, {"Error": "Ruta no existente"})
+            HTTPDataHandler.handle_response(self, 404, {"Error": "la Ruta no existente"})
 
             
+
+  
+
 def run(server_class=HTTPServer, handler_class=PatientHandler, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print(f"Iniciando servidor HTTP en puerto {port}...")
+    print(f"Iniciando servidor {port}...")
     httpd.serve_forever()
 
 if __name__ == '__main__':
